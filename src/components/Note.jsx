10f58e35e2279d40940a3ed51f1ce5e83a53c320
Juha Paavolainen <jpaavola18@student.oulu.fi>
2020-05-27 18:11:06 +0300

    Part 1 of Google Keep clone complete

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+    return(
+        <div className="note">
+        <h1>Title of Note</h1>
+            <p>Content of Note</p>
+        </div>
+    );
+}
+
+export default Note;
